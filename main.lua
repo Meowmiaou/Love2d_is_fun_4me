0bd7c5a5db3dee0b2f1edfa48fe91c6154768119
Meowmiaou <emu.hof@gmail.com>
2014-05-13 18:21:18 +0200

    Butter Update

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ function love.load()
 	world = {}
 	object_count = 0
 	objects.speed = -4
+	love.window.setMode(1500,600,{})
 	width = love.graphics: getWidth()
 	height = love.graphics: getHeight()
 	love.graphics.setBackgroundColor(255,255,255)
@@ -17,23 +18,34 @@ function love.load()
 	char.charge = 0
 	buffer = 0
 	buffermore = true
+	long = 0
 	player = love.graphics.newImage("goop.png")
-	random_pillar()
+	objects.lastpillarsize = 100
 end
 
 function love.update(dt)
 	chargin()
 	object_movement()
 	player_movement()
-	gravity()
-	check_pillar_offscreen()
+	gravity()	
+	--check_pillar_offscreen()
 	collision_hori()
+	collision_verti()
+	pillar_timer()
 	love.draw()
 	buffer = (buffer + 1) % 2
+	long = (long + 1)
 end
 
-function random_pillar(arg)   -- arg is the position in objects the random pillar is going to get
-	spawn_pillar(love.math.random(20,4*(height/6)), arg)
+function random_pillar()   -- arg is the position in objects the random pillar is going to get
+	spawn_pillar(love.math.random(40,4*(height/6)))
+end
+
+function pillar_timer()
+	if long > objects.lastpillarsize then
+		random_pillar()
+		long = 0
+	end
 end
 
 function check_pillar_offscreen( ... )
@@ -74,18 +86,37 @@ function reset()
 	char.charge = 0
 	buffer = 0
 	buffermore = true
-	random_pillar()
+	objects.lastpillarsize = 100
 end
 
 function collision_hori( ... )
 	for  p = 0 , object_count - 1, 1 do
-		if char.x < objects[p].posx and objects[p].posx < char.x + char.width then
-			if char.y + char.height > world.floor - objects[p].height then
+		if char.x <= objects[p].posx and objects[p].posx <= char.x + char.width then
+			if char.y + char.height - 5 > world.floor - objects[p].height then
 				reset()
+				break
+				--world.score = 0
+				--objects[p].gave_score = true
+			elseif objects[p].gave_score == false then 
+				world.score =  world.score + 1
+				objects[p].gave_score = true
+
 			end
 		end
 	end
 end
+
+function collision_verti()
+	for  p = 0 , object_count - 1, 1 do
+		if char.x <= objects[p].posx + objects[p].width and objects[p].posx <= char.x + char.width then
+			if char.y + char.height >= world.floor - objects[p].height then
+				char.y = world.floor - objects[p].height - char.height
+				char.speed = 0
+			end
+		end
+	end
+end
+
 
 function chargin()
 	if love.keyboard.isDown(" ")  then
@@ -120,28 +151,20 @@ function object_movement()
 	end
 end
 
-function collision(x1,y1,w1,h1,x2,y2,w2,h2) --x1,y1 top left coordinate	w1,h1 width and height
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
-end
-
 function draw_char( ... )
 	love.graphics.setColor(255,0,255)
 	love.graphics.rectangle("fill", char.x, char.y, char.width, char.height)
 end
 
-function spawn_pillar(h, argu)
-	if argu == nil then  --if the pillar is new
-		argu = object_count
-		object_count = object_count + 1
-	end
+function spawn_pillar(h)
 	local pillar = {}
 	pillar.posx = width
 	pillar.height = h
+	objects.lastpillarsize = h/4 + 80
 	pillar.width = 50
-	objects[argu] = pillar
+	pillar.gave_score = false
+	objects[object_count] = pillar
+	object_count = object_count + 1
 end
 
 function draw_objects()
